1146580706456fb3cea6e31c415ee30d51fb8011
MrGoropeza <goropeza8@gmail.com>
2022-11-11 19:38:43 -0300

    update: cambios en el servicio de temas, pero no hace nada

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -13,9 +13,11 @@ export class ThemeService {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.getItem("theme") === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
       document.documentElement.classList.add('dark');
+      document.documentElement.setAttribute("data-theme", "dark");
       this.theme = "dark";
     } else {
       document.documentElement.classList.remove('dark');
+      document.documentElement.setAttribute("data-theme", "default");
       this.theme = "light";
     }
   }
