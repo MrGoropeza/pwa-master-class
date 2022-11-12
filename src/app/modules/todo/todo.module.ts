dc4b6b85a304eff8ab32317fe7100c8fef4b147b
MrGoropeza <goropeza8@gmail.com>
2022-11-11 22:59:03 -0300

    delete: el componente todo-list-tile

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -4,7 +4,6 @@ import { TodoMainComponent } from './pages/todo-main/todo-main.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoListTileComponent } from './components/todo-list-tile/todo-list-tile.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { PrimeComponentsModule } from 'src/app/prime-components.module';
@@ -22,7 +21,6 @@ const routes: Routes = [
     TodoMainComponent,
     HeaderComponent,
     TodoListComponent,
-    TodoListTileComponent,
     TodoComponent,
     TodoFormComponent
   ],
