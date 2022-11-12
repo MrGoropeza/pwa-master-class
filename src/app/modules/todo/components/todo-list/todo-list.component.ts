d2ae8b368f2dabb8ceb76545cc8c6ad344b851f5
MrGoropeza <goropeza8@gmail.com>
2022-11-12 18:13:57 -0300

    update: lista de tareas ahora es dinamico

--- src/app/modules/todo/components/todo-list/todo-list.component.ts
+++ src/app/modules/todo/components/todo-list/todo-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { TodoModel } from 'src/app/models/todo.model';
+import { GetTodos } from '../../store/actions/get-todo.actions';
+import { TodoState } from '../../store/reducers/todo.reducer';
+import { getTodos } from '../../store/selectors/todo.selectors';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +13,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  todos!: Observable<TodoModel[]>
+
+  constructor(private todoStore: Store<TodoState>) { }
 
   ngOnInit(): void {
+    this.todoStore.dispatch(GetTodos());
+    this.todos = this.todoStore.select(getTodos);
   }
 
 }
