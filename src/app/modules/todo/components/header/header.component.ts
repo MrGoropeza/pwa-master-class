0d9e61a7add348e805ab7fb0d869974a757f8141
MrGoropeza <goropeza8@gmail.com>
2022-11-12 18:07:09 -0300

    update: listo la funcionalidad del header comp

--- src/app/modules/todo/components/header/header.component.ts
+++ src/app/modules/todo/components/header/header.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { InputSwitchOnChangeEvent } from 'primeng/inputswitch';
+import { Observable } from 'rxjs';
+import { UserModel } from 'src/app/models/user.model';
+import { CheckAuths } from 'src/app/modules/auth/store/actions/check-auth.actions';
+import { SignOuts } from 'src/app/modules/auth/store/actions/sign-out.actions';
+import { State } from 'src/app/modules/auth/store/reducer/auth.reducer';
+import { getAuthUser } from 'src/app/modules/auth/store/selector/auth.selectors';
 import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
@@ -9,21 +16,30 @@ import { ThemeService } from 'src/app/services/theme.service';
 })
 export class HeaderComponent implements OnInit {
 
+  user!: Observable<UserModel | null>;
+
   isOnDarkTheme: boolean;
 
   constructor(
-    private themeService: ThemeService
+    private themeService: ThemeService,
+    private authStore: Store<State>
   ) { 
     themeService.setOSTheme();
     this.isOnDarkTheme = themeService.getTheme() === "dark";
   }
 
   ngOnInit(): void {
+    this.authStore.dispatch(CheckAuths());
+    this.user = this.authStore.select(getAuthUser);
   }
   
   themeSwitchClick(event: InputSwitchOnChangeEvent){
     if(event.checked){this.themeService.setDarkTheme()}
     else{this.themeService.setLightTheme()}
+  }
+
+  signout(){
+    this.authStore.dispatch(SignOuts());
   }
   
 }
