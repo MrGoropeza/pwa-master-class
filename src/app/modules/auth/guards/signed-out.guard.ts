db2d1803cb7933aa7ffbdb43611783af35feed2b
MrGoropeza <goropeza8@gmail.com>
2022-11-12 00:20:36 -0300

    add: guards de rutas para la sesion

--- src/app/modules/auth/guards/signed-out.guard.ts
+++ src/app/modules/auth/guards/signed-out.guard.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignedOutGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      return this.authService.checkAuth().pipe(
+        map((user) => {
+          return user === null;
+        }),
+        tap((res) => {
+          if(res){
+            return route;
+          }else{
+            return this.router.navigate(["/todos"]);
+          }
+        })
+      );
   }
   
 }
